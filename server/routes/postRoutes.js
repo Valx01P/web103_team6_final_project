b44a614569a8f9426f5020e3073489b811461687
Pablo Valdes <pablovaldes0925@gmail.com>
2024-11-14 01:26:26 -0500

    small fix

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express'
 import postController from '../controllers/postController.js'
 import verifyJWT from '../middleware/verifyJWT.js'
-import verifyUserOwnership from '../middleware/verifyUserOwnership.js'
 
 const router = express.Router()
 
 router.route('/')
     .get(verifyJWT, postController.getAllPosts)
-    
+
 
 export default router

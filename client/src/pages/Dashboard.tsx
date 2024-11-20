39dbf70701efa2ade78b26c726e2ad44889133e2
Pablo Valdes <pablovaldes0925@gmail.com>
2024-11-19 22:24:41 -0500

    layout options sorta

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,8 +1,38 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { Post, NewPostData } from "../types/db"
 
 const Dashboard = () => {
   const [closeSidebar, setCloseSidebar] = useState(false)
   const [activeContent, setActiveContent] = useState('posts')
+  const [posts, setPosts] = useState([] as Post[])
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [tags, setTags] = useState([] as string[])
+  const [layoutMenu, setLayoutMenu] = useState(false)
+  const [layoutType, setLayoutType] = useState(localStorage.getItem('layout') || 'medium')
+
+  useEffect(() => {
+    try {
+      const fetchPosts = async () => {
+        const response = await fetch('http://localhost:3000/posts', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
+          },
+          credentials: 'include',
+        })
+        console.log('RES',response)
+        const data = await response.json()
+        console.log('DATA',data)
+        setPosts(data.posts)
+        console.log('POSTS',posts)
+      }
+      fetchPosts()
+    } catch (error) {
+      console.error('Failed to fetch posts:', error)
+    }
+  }, [])
 
   const handleSidebar = () => {
     setCloseSidebar(!closeSidebar)
@@ -10,6 +40,39 @@ const Dashboard = () => {
 
   const handleContent = (content: string) => {
     setActiveContent(content)
+  }
+
+  const handleLayout = () => {
+    setLayoutMenu(!layoutMenu)
+  }
+
+  const handleNewPost = async (e: React.FormEvent) => {
+    e.preventDefault()
+    const newPostData: NewPostData = {
+      post: {
+        title,
+        content,
+      },
+      tags,
+    }
+    try {
+      const response = await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
+        },
+        credentials: 'include',
+        body: JSON.stringify(newPostData),
+      })
+      console.log('RES',response)
+      const data = await response.json()
+      console.log('DATA',data)
+      setPosts([...posts, data.post])
+      console.log('POSTS',posts)
+    } catch (error) {
+      console.error('Failed to create post:', error)
+    }
   }
 
   return (
@@ -24,7 +87,15 @@ const Dashboard = () => {
                 { (activeContent === 'posts' || activeContent === 'tags') && <h1 className="text-3xl text-white hover:text-blue-300 cursor-pointer" onClick={() => handleContent('create')}>Create Problem</h1> }
                 { (activeContent === 'tags' || activeContent === 'create') && <h1 className="text-3xl text-white hover:text-blue-300 cursor-pointer" onClick={() => handleContent('posts')}>View Posts</h1> }
                 { (activeContent === 'posts' || activeContent === 'create') && <h1 className="text-3xl text-white hover:text-blue-300 cursor-pointer" onClick={() => handleContent('tags')}>View Tags</h1> }
-                <h1 className="text-3xl text-white hover:text-blue-300">Layout</h1>
+                <h1 className="text-3xl text-white hover:text-blue-300 cursor-pointer" onClick={handleLayout}>Layout</h1>
+                { layoutMenu && (
+                  <div className="flex flex-col gap-4 ml-4">
+                    <h1 className="text-2xl text-white hover:text-blue-300 cursor-pointer" onClick={() => { setLayoutType('small'); localStorage.setItem('layout', 'small') }}>⋅ Small</h1>
+                    <h1 className="text-2xl text-white hover:text-blue-300 cursor-pointer" onClick={() => { setLayoutType('medium'); localStorage.setItem('layout', 'medium') }}>⋅ Medium</h1>
+                    <h1 className="text-2xl text-white hover:text-blue-300 cursor-pointer" onClick={() => { setLayoutType('large'); localStorage.setItem('layout', 'large') }}>⋅ Large</h1>
+                  </div>
+                )  
+                }
               </>
             )
           }
@@ -33,9 +104,45 @@ const Dashboard = () => {
       </div>
       {/* Main Content */}
       <div className="bg-zinc-600 min-h-full w-full">
-          { activeContent === 'posts' && ( <h1>posts</h1> ) }
-          { activeContent === 'tags' && ( <h1>tags</h1> ) }
-          { activeContent === 'create' && ( <h1>layout</h1> ) }
+          { activeContent === 'posts' && ( <div className={`${layoutType === 'small' && ('grid grid-cols-4')} ${layoutType === 'large' && ('grid grid-cols-2')}`}>
+            {
+              posts.map((post: Post) => (
+                <div key={post.id} className="bg-zinc-800 p-4 m-4 rounded-lg">
+                  <h1 className="text-2xl text-white">{post.title}</h1>
+                  <p className="text-white">{post.content}</p>
+                </div>
+              ))  
+            }
+          </div> ) }
+          { activeContent === 'tags' && ( <div>
+            <h1>tags</h1>
+          </div> ) }
+          { activeContent === 'create' && ( <div className="text-black w-full min-h-full flex align-middle justify-center items-center ">
+            <form onSubmit={handleNewPost} className="p-8 bg-zinc-800 w-[400px] flex flex-col gap-4">
+              <label className="text-white text-xl">Create Post</label>
+              <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="border border-gray-400 p-2 w-full"
+              />
+              <textarea
+                placeholder="Content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                className="border border-gray-400 p-2 w-full"
+              />
+              <input
+                type="text"
+                placeholder="Tags"
+                value={tags}
+                onChange={(e) => setTags(e.target.value.split(','))}
+                className="border border-gray-400 p-2 w-full"
+              />
+              <button type="submit" className="bg-blue-600 text-white p-2">Create Post</button>
+            </form>
+          </div> ) }
 
       </div>
     </div>
--- client/src/types/db.ts
+++ client/src/types/db.ts
@@ -23,3 +23,70 @@ export interface SignInData {
     email: string
     password: string
 }
+
+/*
+ex. new post data
+
+{
+    "post": {
+        "title": "ex post w tags",
+        "content": "dope post"
+    },
+    "tags": ["bro", "lmao"]
+}
+
+*/
+
+export interface NewPostData {
+    post: {
+        title: string
+        content: string
+    }
+    tags: string[]
+}
+
+/*
+ex. post data
+    "posts": [
+        {
+            "id": 10,
+            "user_id": "fee43ec7-566d-4de7-a32e-4344b54b99a8",
+            "title": "ex post w tags",
+            "content": "dope post",
+            "created_at": "2024-11-20T07:42:27.081Z",
+            "score": 0,
+            "last_updated": "2024-11-20T07:42:27.081Z",
+            "tags": [
+                "lmao",
+                "bro"
+            ]
+        },
+        {
+            "id": 11,
+            "user_id": "fee43ec7-566d-4de7-a32e-4344b54b99a8",
+            "title": "ayooo",
+            "content": "epic post",
+            "created_at": "2024-11-20T07:44:16.426Z",
+            "score": 0,
+            "last_updated": "2024-11-20T07:44:16.426Z",
+            "tags": [
+                "gyattt"
+            ]
+        }
+    ]
+*/
+
+export interface Post {
+    id: number
+    user_id: string
+    title: string
+    content: string
+    created_at: string
+    score: number
+    last_updated: string
+    tags: string[]
+}
+
+export interface Posts {
+    posts: Post[]
+}
